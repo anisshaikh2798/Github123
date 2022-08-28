1214bcb8f425aa5e84fb9b8b670979d17729a724
nc <nc@DESKTOP-H0G03DT>
2022-08-28 09:40:22 +0530

    [ANIS1]

---
+++ src/DEMO/SAMPLE1.java
@@ -0,0 +1,15 @@
+package DEMO;
+
+public class SAMPLE1 {
+	public static void main(String[] args) {
+		
+	
+	
+	System.out.println("I Love My Country So Much");
+	
+	
+	System.out.println("I Love My Father and Mother");
+	
+}
+
+}
