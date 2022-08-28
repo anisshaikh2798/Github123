553870da3256654f5ba272992a55784e7d5afcce
nc <nc@DESKTOP-H0G03DT>
2022-08-28 20:46:37 +0530

    [ANIS1]

---
+++ src/DEMO/SAMPLE2.java
@@ -0,0 +1,15 @@
+package DEMO;
+
+public class SAMPLE2
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("We all are friends");
+		
+		
+		
+		System.out.println("India Is My Country");
+		
+	}
+
+}
